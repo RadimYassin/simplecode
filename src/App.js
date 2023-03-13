ee769c356a3a74b2ab7d6266c97e66a0dc2db695
Ghizlane <khabir.ghizlane@ofppt-edu.ma>
2023-03-14 00:08:30 +0100

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LandingPage from './LandingPage';
 function App() {
   return (
-    <div className="App">
-         <h1>helloapollo</h1>
-    </div>
+    <>
+      <LandingPage />
+    </>
   );
 }
 
