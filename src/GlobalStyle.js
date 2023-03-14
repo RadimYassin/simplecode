0a4d0f94cef96788a34fac99874f5141ecba331a
RadimYassin <radimyassin20@gamil.com>
2023-03-14 12:20:57 +0100

    create Globalstyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,36 @@
+import styled ,{createGlobalStyle} from "styled-components";
+
+
+ const GlobleStyle=createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:sans-serif;
+}
+
+
+
+
+`
+export default GlobleStyle
+export const  Container=styled.div`
+
+
+z-index:1;
+width:100%;
+max-width:1300px;
+margin-left:auto;
+margin-right:auto;
+padding-left:50px;
+padding-right:50px;
+
+@media screen and (max-width:991px) {
+    padding-left:30px;
+padding-right:30px;
+}
+
+
+
+`
+
